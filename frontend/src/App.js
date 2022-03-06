9dc8d4824fee93e3dd9552573cda4200a4010182
Purnima-1 <baranwal.purnimagmail.com>
2022-03-06 15:34:46 -0800

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <main>
+
+     <h1>My Events</h1>
+    
+    </main>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/events.js
@@ -0,0 +1,41 @@
+const events = [
+    {
+        _id: '1',
+        title: 'Seattle Night Market',
+        image: '/images/id_1.jpg',
+        description:
+          "Seattle's largest 21+ only indoor Night Market returns for the ultimate night of eating, drinking & shopping local at Magnuson Hangar 30.",
+        schedule: 'Sat, Mar 19, 2:00 PM',
+        Address: 'Magnuson Park Hangar 30 • Seattle, WA',
+        price: 'Starts at $12.50',
+        name: 'Seattle Street Food Festival',
+        followers: 517
+        
+      },
+      {
+        _id: '2',
+        title: "YACHT ROCK - 90'S BODACIOUS BOAT PARTY!",
+        image: '/images/id_2.jpg',
+        description:
+          "YACHT ROCK -90s BODACIOUS BOAT PARTY. 90's fashion Dress-Up and Dance with music hits from the 90s and today while touring Seattle!!!",
+        schedule: 'Sat, Mar 12, 7:00 PM',
+        Address: '1611 Fairview Ave E • Seattle, WA',
+        price: 'Starts at $30.00',
+        name: 'Surreal Events- Seattles premier promoter for fun events for 21+ adults',
+        followers: 37
+        },
+      {
+        _id: '3',
+        title: 'Seattle Tech Career Fair:Exclusive Tech Hiring Event-New Tickets Available',
+        image: '/images/id_1.jpg',
+        description:
+          "Seattle's largest 21+ only indoor Night Market returns for the ultimate night of eating, drinking & shopping local at Magnuson Hangar 30.",
+        schedule: 'Mon, Mar 28, 9:00 AM',
+        Address: 'TBD • Seattle, WA',
+        price: 'Free',
+        name: 'Tech Career Fair',
+        followers: '12.2k'
+        
+      }
+      
+]
