ed856e469fbe026b7acbcb5ec404c9284b343cf1
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-03-08 13:17:17 -0800

    Header/ react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-
+import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -9,9 +9,9 @@ function App() {
     <>
     <Header/>
     <main>
-
-     <h1>My Events</h1>
-    
+     <Container>
+       <h1>My Events</h1>
+     </Container>
     </main>
     <Footer/>
     </>
