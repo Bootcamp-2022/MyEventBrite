bba4d097961693309ad7a196c6092d49ce146f85
Purnima-1 <baranwal.purnimagmail.com>
2022-04-06 13:12:14 -0700

    qty functionality added to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Container,Navbar,Nav} from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart;
   return (
     <header>
     <Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
@@ -14,7 +18,7 @@ const Header = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <LinkContainer to= "/tickets">
-            <Nav.Link ><i className="fa-solid fa-ticket"></i> Tickets</Nav.Link>
+            <Nav.Link >{cartItems.reduce((acc, item) => acc + item.qty1, 0)}<i className="fa-solid fa-ticket"></i> Tickets</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/login">
             <Nav.Link ><i className='fas fa-user'></i> Sign In</Nav.Link>
