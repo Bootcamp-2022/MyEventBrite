f362947ea36396e43721bbdd23b8bd0ec24b7f4f
mlynn0791 <99066117+mlynn0791@users.noreply.github.com>
2022-04-27 01:06:52 -0400

    Prepared to Deploy

--- backend/server.js
+++ backend/server.js
@@ -19,19 +19,24 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.get('/api/config/paypal', (req,res)=>
     res.send(process.env.PAYPAL_CLIENT_ID))
+
+    const __dirname = path.resolve()
+
+    if (process.env.NODE_ENV === 'production'){
+      app.use(express.static(path.join(__dirname, '/frontend/build')))
+    
+      app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+      })
+    }else {
+      app.get('/', (req, res) => {
+        res.send('API is running....')
+      })
+    }
+
 app.use(notFound)
 app.use(errorHandler)
 //app.use(protect)
-
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'))
-    
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
-   
-}
-
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT} `));
