acf4738c7706edd24fdbcc8fb8f45e74fa2b8756
mlynn0791 <99066117+mlynn0791@users.noreply.github.com>
2022-04-27 20:27:39 -0400

    Prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,94 @@
+// import express from 'express'
+// import dotenv from 'dotenv'
+// import connectDB from './config/db.js'
+// import eventRoutes from './routes/eventRoutes.js'
+// import orderRoutes from './routes/orderRoutes.js'
+// import userRoutes from './routes/userRoutes.js'
+// import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+
+// //import { protect } from './middleware/authMiddleware.js'
+
+// const app = express()
+// dotenv.config()
+// connectDB()
+
+
+// app.use(express.json())
+// app.use('/api/events', eventRoutes)
+// app.use('/api/users', userRoutes)
+// app.use('/api/orders', orderRoutes)
+// app.get('/api/config/paypal', (req,res)=>
+//     res.send(process.env.PAYPAL_CLIENT_ID))
+
+//     const __dirname = path.resolve()
+
+//     if (process.env.NODE_ENV === 'production'){
+//       app.use(express.static(path.join(__dirname, '/frontend/build')))
+    
+//       app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//       })
+//     }else {
+//       app.get('/', (req, res) => {
+//         res.send('API is running....')
+//       })
+//     }
+
+    
+// app.use(notFound)
+// app.use(errorHandler)
+// //app.use(protect)
+
+// // if (process.env.NODE_ENV === 'production'){
+// //     app.use(express.static('frontend/build'))
+    
+// //     app.get("*", (req, res) => {
+// //         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+// //     })
+   
+// // }
+
+
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT, console.log(`Server is running on port ${PORT} `));
+
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import connectDB from './config/db.js'
 import eventRoutes from './routes/eventRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-import { errorHandler, notFound } from './middleware/errorMiddleware.js'
-import res from 'express/lib/response'
-//import { protect } from './middleware/authMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const app = express()
 dotenv.config()
 connectDB()
 
-
 app.use(express.json())
 app.use('/api/events', eventRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
-app.get('/api/config/paypal', (req,res)=>
-    res.send(process.env.PAYPAL_CLIENT_ID))
 
-    const __dirname = path.resolve()
+app.get('/api/config/paypal', (req, res) => 
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
-    if (process.env.NODE_ENV === 'production'){
-      app.use(express.static(path.join(__dirname, '/frontend/build')))
-    
-      app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-      })
-    }else {
-      app.get('/', (req, res) => {
-        res.send('API is running....')
-      })
-    }
 
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 app.use(notFound)
 app.use(errorHandler)
-//app.use(protect)
-
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server is running on port ${PORT} `));
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
